50d192872032176c65bf6eb37a38e55190b66041
Shivam <devshivam018@gmail.com>
2025-07-17 00:56:02 +0530

    added api route

---
+++ app/routes/hello.tsx
@@ -0,0 +1,23 @@
+import { LoaderFunction } from "@remix-run/node";
+import { getPostgresDatabaseManager } from "~/submodule-database-manager/postgresDatabaseManager.server";
+
+export const loader: LoaderFunction = async () => {
+
+    const postgresManager = await getPostgresDatabaseManager(null);
+    if (postgresManager.success === false) {
+        return postgresManager.err.message;
+    }
+
+    const query = 'Select * from public.doctors;'
+
+    const result = await postgresManager.data.execute(query);
+
+    console.log(result.success);
+    if(result.success===false){
+        console.log(result.err);
+    }else{
+        console.log(result.data);
+    }
+    return "hello"
+        ;
+}
